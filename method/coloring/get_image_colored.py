575eb4a25e32e559782a3a532b9e7db1d6d2899a
張仕瑞（Shirui） <shirui.zhang.japan@gmail.com>
2019-12-25 02:08:27 +0800

    11

--- method/coloring/get_image_colored.py
+++ method/coloring/get_image_colored.py
@@ -40,30 +40,33 @@ def read_image(args, image_current):
         ).split('_')[0] + '_tiff'
 
     # read image
-    image_index = image_coordinate[2]
-    path_image = os.path.join(
-        args.dir_image,
-        name_subset,
-        image_current['seriesuid'],
-        'whole_image',
-        'whole_{image_index}.tiff'.format(image_index=int(image_index))
-        )
+    bias_z = int(image_current['diameter'] / (2 * image_current['spacing_z']))
+    image_index = int(image_coordinate[2])
 
-    image = cv2.imread(path_image, flags=2)
-    cv2.imwrite(args.path_image_colored, image)
+    for idx in range(image_index - bias_z, image_index + bias_z + 1):
+        path_image = os.path.join(
+            args.dir_image,
+            name_subset,
+            image_current['seriesuid'],
+            'whole_image',
+            'whole_{idx}.tiff'.format(idx=idx)
+            )
 
-    # cut the image
-    x_start = int(image_coordinate[1] - args.size_cutting / 2)
-    x_end = int(image_coordinate[1] + args.size_cutting / 2)
-    y_start = int(image_coordinate[0] - args.size_cutting / 2)
-    y_end = int(image_coordinate[0] + args.size_cutting / 2)
+        image = cv2.imread(path_image, flags=2)
+        cv2.imwrite(args.path_image_colored, image) # test
 
-    image = image[x_start: x_end, y_start: y_end]
+        # cut the image
+        x_start = int(image_coordinate[1] - args.size_cutting / 2)
+        x_end = int(image_coordinate[1] + args.size_cutting / 2)
+        y_start = int(image_coordinate[0] - args.size_cutting / 2)
+        y_end = int(image_coordinate[0] + args.size_cutting / 2)
 
-    cv2.imwrite(args.path_image_colored, image)
+        image = image[x_start: x_end, y_start: y_end]
 
-    # append to the list
-    list_image.append(image)
+        cv2.imwrite(args.path_image_colored, image) # test
+
+        # append to the list
+        list_image.append(image)
 
     return list_image
 

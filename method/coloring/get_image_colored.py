45505aa52133d2da367d5984fd1f1114b85dd316
張仕瑞（Shirui） <shirui.zhang.japan@gmail.com>
2019-12-24 21:16:15 +0800

    qq

--- method/coloring/get_image_colored.py
+++ method/coloring/get_image_colored.py
@@ -2,48 +2,39 @@ import argparse
 import os
 import pprint
 import sys
+import pandas as pd
+import random
 
 sys.path.append(os.getcwd())
-from pre_processing.utility import get_image_info, world2voxel_coord
+from pre_processing.utility import get_image_info, get_coordinate
 
 
 def argument():
 
+    # default value
     default_path_info = os.path.join(os.getcwd(), 'data', 'info', 'info_luna16.csv')
     default_dir_image = os.path.join(os.getcwd(), 'data', 'image')
     default_path_image_colored = os.path.join(
         os.path.dirname(__file__), 'test', 'image_colored.{format_image}'.format(format_image='png'))
 
+    # add argument
     parser = argparse.ArgumentParser()
     parser.add_argument('--path-info', default=default_path_info, type=str)
     parser.add_argument('--dir-image', default=default_dir_image, type=str)
-    parser.add_argument('--size-cutting', default=32)
-
+    parser.add_argument('--size-cutting', default=32, type=int)
     parser.add_argument('--path-image-colored', default=default_path_image_colored, type=str)
 
+    # parse argument
     args = parser.parse_args()
     return args
 
-def get_coordinate(list_image):
-    voxel_coord_x = world2voxel_coord(
-        current_item['coord_x'],
-        current_item['origin_x'],
-        current_item['spacing_x'],
-        )
-    voxel_coord_y = world2voxel_coord(
-        current_item['coord_y'],
-        current_item['origin_y'],
-        current_item['spacing_y'],
-        )
-    voxel_coord_z = world2voxel_coord(
-        current_item['coord_z'],
-        current_item['origin_z'],
-        current_item['spacing_z'],
-        )
-    voxel_coord_x = round(voxel_coord_x)
-    voxel_coord_y = round(voxel_coord_y)
-    voxel_coord_z = round(voxel_coord_z)
-
 if __name__ == '__main__':
     args = argument()
+
+    info_luna16 = pd.read_csv(args.path_info, index_col=0)
+    list_info_image = get_image_info(info_luna16)
+
+    current_image = random.choice(list_info_image)
+    get_coordinate = get_coordinate(current_image)
+
     print('test')
--- pre_processing/utility.py
+++ pre_processing/utility.py
@@ -1,30 +1,55 @@
+import sys
+import pprint
+import numpy as np
 # utility for LUNA16
 
-def world2voxel_coord(worldCoord, origin, spacing):
-    stretchedVoxelCoord = np.absolute(worldCoord - origin)
-    voxelCoord = stretchedVoxelCoord / spacing
-    return voxelCoord
+def world2voxel_coord(world_coord, origin, spacing):
+    stretched_voxel_coord = np.absolute(world_coord - origin)
+    voxel_coord = stretched_voxel_coord / spacing
+    return voxel_coord
+
+def get_coordinate(current_item):
+    voxel_coord_x = world2voxel_coord(
+        current_item['coord_x'],
+        current_item['origin_x'],
+        current_item['spacing_x'],
+        )
+    voxel_coord_y = world2voxel_coord(
+        current_item['coord_y'],
+        current_item['origin_y'],
+        current_item['spacing_y'],
+        )
+    voxel_coord_z = world2voxel_coord(
+        current_item['coord_z'],
+        current_item['origin_z'],
+        current_item['spacing_z'],
+        )
+    voxel_coord_x = round(voxel_coord_x)
+    voxel_coord_y = round(voxel_coord_y)
+    voxel_coord_z = round(voxel_coord_z)
+
+    return voxel_coord_x, voxel_coord_y, voxel_coord_z
 
 def get_image_info(input_pd):
     list_data = []
     for each_data in input_pd.iterrows():
-        path_seriesuid_folder = each_data[1][0]
-        seriesuid = each_data[1][1]
-        each_class = each_data[1][6]
+        path_seriesuid_folder = each_data[1]['path_seriesuid_folder']
+        seriesuid = each_data[1]['seriesuid']
+        _class = each_data[1]['class']
 
-        coord_x = each_data[1][2]
-        coord_y = each_data[1][3]
-        coord_z = each_data[1][4]
+        coord_x = each_data[1]['coord_x']
+        coord_y = each_data[1]['coord_y']
+        coord_z = each_data[1]['coord_z']
 
-        origin_x = each_data[1][8]
-        origin_y = each_data[1][9]
-        origin_z = each_data[1][10]
+        origin_x = each_data[1]['origin_x']
+        origin_y = each_data[1]['origin_y']
+        origin_z = each_data[1]['origin_z']
 
-        spacing_x = each_data[1][11]
-        spacing_y = each_data[1][12]
-        spacing_z = each_data[1][13]
+        spacing_x = each_data[1]['spacing_x']
+        spacing_y = each_data[1]['spacing_y']
+        spacing_z = each_data[1]['spacing_z']
 
-        diameter = each_data[1][5]
+        diameter = each_data[1]['diameter']
 
         if sys.platform == 'darwin':
             path_seriesuid_folder = path_seriesuid_folder.replace('\\', '/')
@@ -32,7 +57,7 @@ def get_image_info(input_pd):
         dict_append = {
             'path_seriesuid_folder': path_seriesuid_folder,
             'seriesuid': seriesuid,
-            'class': each_class,
+            'class': _class,
 
             'coord_x': coord_x,
             'coord_y': coord_y,
@@ -49,3 +74,4 @@ def get_image_info(input_pd):
             'diameter': diameter,
         }
         list_data.append(dict_append)
+    return list_data

dcdb14a793306b920b891f059ff59ec80c58e94b
張仕瑞（Shirui） <shirui.zhang.japan@gmail.com>
2020-01-01 18:26:38 +0800

    11

--- method/cnn_attention_area/main.py
+++ method/cnn_attention_area/main.py
@@ -1,5 +1,7 @@
-from __future__ import print_function
-
+'''
+1) get the attention area by VAE model
+2) model cnn part is x-ception (, maybe)
+'''
 import argparse
 import os
 import sys
@@ -14,47 +16,42 @@ from torch.nn import functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 
-
-#d append sys.path
+# append sys.path
 sys.path.append(os.getcwd())
+
 from pre_processing.utility import get_coordinate, get_image_info
+from utility.auto_encoding_variational import VAE
 
 
 # define argument
 def argument():
-    parser = argparse.ArgumentParser(description='VAE for utility')
+    parser = argparse.ArgumentParser()
     parser.add_argument('--path-input', default=None)
+    parser.add_argument('--path-model-vae', default=None)
     parser.add_argument('--dir-image', type=str)
-    parser.add_argument('--size-cutting', default=28)
-    parser.add_argument('--dimension-latent', type=int, default=20)
+    parser.add_argument('--size-cutting', default=50)
 
     parser.add_argument('--rate-train', default=0.9, type=float)
-    parser.add_argument('--size-batch', type=int, default=128, metavar='N',
+    parser.add_argument('--size-batch', type=int, default=128,
         help='input batch size for train (default: 128)')
-    parser.add_argument('--epoch', type=int, default=10, metavar='N',
+    parser.add_argument('--epoch', type=int, default=10,
         help='number of epoch to train (default: 10)')
 
     parser.add_argument('--no-cuda', action='store_true', default=False,
         help='enables CUDA train')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
+    parser.add_argument('--seed', type=int, default=1,
         help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-        help='how many batches to wait before logging train status')
 
     parser.add_argument('--num-cross', default=None, type=int)
     parser.add_argument('--use-cross', default=None, type=int)
 
-    parser.add_argument('--path-save-model', default=os.path.join(os.getcwd(), 'utility/model/model_vae.pt'), type=str,
-        help='set the path of model to save')
-
     args = parser.parse_args()
-    args.cuda = not args.no_cuda and torch.cuda.is_available()
+    cuda = not args.no_cuda and torch.cuda.is_available()
 
     torch.manual_seed(args.seed)
-    device = torch.device("cuda" if args.cuda else "cpu")
-    kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-
-    return args, device, kwargs
+    device = torch.device("cuda" if cuda else "cpu")
+    args.device = device
+    return args
 
 # define data set
 class DatasetTrain():
@@ -92,6 +89,7 @@ class DatasetTrain():
 
         image = image[x_start: x_end, y_start: y_end]
         image = np.expand_dims(image, 0)
+
         return image
 
 class DatasetTest():
@@ -131,56 +129,106 @@ class DatasetTest():
         image = np.expand_dims(image, 0)
         return image
 
-# define class 
-class VAE(nn.Module):
-    def __init__(self, args):
-        super(VAE, self).__init__()
+class CnnModel(nn.Module):
+    def __init__(self):
+        super().__init__()
 
-        self.fc1 = nn.Linear(int(args.size_cutting * args.size_cutting), 400)
-        self.fc21 = nn.Linear(400, int(args.dimension_latent))
-        self.fc22 = nn.Linear(400, int(args.dimension_latent))
-        self.fc3 = nn.Linear(int(args.dimension_latent), 400)
-        self.fc4 = nn.Linear(400, int(args.size_cutting * args.size_cutting))
+        # input 2 * 50 * 50
 
-        self.args = args
+        self.conv_1 = nn.Conv2d(
+            in_channels=2, out_channels=20, kernel_size=7, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        ) # 20 * 44 * 44
 
-    def encode(self, x):
-        h1 = F.relu(self.fc1(x))
-        return self.fc21(h1), self.fc22(h1)
+        self.pooling_1 = nn.MaxPool2d(
+            kernel_size=2, stride=2, padding=0, dilation=1, return_indices=False, ceil_mode=False
+        ) # 20 * 22 * 22
 
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5*logvar)
-        eps = torch.randn_like(std)
-        return mu + eps*std
+        self.conv_2 = nn.Conv2d(
+            in_channels=20, out_channels=50, kernel_size=7, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        ) # 50 * 16 * 16
 
-    def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
+        self.pooling_2 = nn.MaxPool2d(
+            kernel_size=2, stride=2, padding=0, dilation=1, return_indices=False, ceil_mode=False
+        ) # 50 * 8 * 8
 
-    def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, self.args.size_cutting * self.args.size_cutting))
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        self.conv_3 = nn.Conv2d(
+            in_channels=50, out_channels=500, kernel_size=7, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        ) # 500 * 2 * 2
 
-# Reconstruction + KL divergence losses summed over all elements and batch
-def loss_function(recon_x, x, mu, logvar, args):
-    BCE = F.binary_cross_entropy(recon_x, x.view(-1, args.size_cutting * args.size_cutting), reduction='sum')
+        self.pooling_3 = nn.MaxPool2d(
+            kernel_size=2, stride=2, padding=0, dilation=1, return_indices=False, ceil_mode=False
+        ) # 500 * 1 * 1
 
-    # see Appendix B from VAE paper:
-    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-    # https://arxiv.org/abs/1312.6114
-    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        # activate fuction ReLU layer
 
-    return BCE + KLD
+        self.conv_4 = nn.Conv2d(
+            in_channels=500, out_channels=2, kernel_size=1, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        ) # 2 * 1 * 1
 
-def train(model, train_loader, epoch, device, args):
+        '''
+        # original model
+        # input 1* 50 * 50
+
+        self.conv_1 = nn.Conv2d(
+            in_channels=1, out_channels=20, kernel_size=7, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        ) # 20 * 44 * 44
+
+        self.pooling_1 = nn.MaxPool2d(
+            kernel_size=2, stride=2, padding=0, dilation=1, return_indices=False, ceil_mode=False
+        ) # 20 * 22 * 22
+
+        self.conv_2 = nn.Conv2d(
+            in_channels=20, out_channels=50, kernel_size=7, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        ) # 50 * 16 * 16
+
+        self.pooling_2 = nn.MaxPool2d(
+            kernel_size=2, stride=2, padding=0, dilation=1, return_indices=False, ceil_mode=False
+        ) # 50 * 8 * 8
+
+        self.conv_3 = nn.Conv2d(
+            in_channels=50, out_channels=500, kernel_size=7, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        ) # 500 * 2 * 2
+
+        self.pooling_3 = nn.MaxPool2d(
+            kernel_size=2, stride=2, padding=0, dilation=1, return_indices=False, ceil_mode=False
+        ) # 500 * 1 * 1
+
+        # activate fuction ReLU layer
+
+        self.conv_4 = nn.Conv2d(
+            in_channels=500, out_channels=2, kernel_size=1, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        ) # 2 * 1 * 1
+        '''
+
+    def forward(self, input_image):
+        out = self.conv_1(input_image) # 20 * 44 * 44
+        out = self.pooling_1(out) # 20 * 22 * 22
+
+        out = self.conv_2(out) # 50 * 16 * 16
+        out = self.pooling_2(out) # 50 * 8 * 8
+
+        out = self.conv_3(out) # 500 * 2 * 2
+        out = self.pooling_3(out) # 500 * 1 * 1
+
+        out_relu = F.relu(out)
+
+        # out = F.dropout(out_relu)
+        out = self.conv_4(out) # 2 * 1 * 1
+
+        return out, out_relu
+
+def train(model, optimizer, model_vae, train_loader, epoch, device, args):
     model.train()
     train_loss = 0
     for batch_idx, data in enumerate(train_loader):
         data = data.to(device, dtype= torch.float)
+
+        # get attention area
+        attention_area = model_vae(data)
+
+        # train the model
         optimizer.zero_grad()
-        recon_batch, mu, logvar = model(data)
+        recon_batch, mu, logvar = model(data, attention_area)
         loss = loss_function(recon_batch, data, mu, logvar, args)
         loss.backward()
         train_loss += loss.item()
@@ -195,14 +243,20 @@ def train(model, train_loader, epoch, device, args):
     print('====> Epoch: {} Average loss: {:.4f}'.format(
           epoch, train_loss / len(train_loader.dataset)))
 
-def test(model, test_loader, epoch, device, args):
+def test(model, optimizer, model_vae, test_loader, epoch, device, args):
     model.eval()
     test_loss = 0
     with torch.no_grad():
         for i, data in enumerate(test_loader):
             data = data.to(device, dtype= torch.float)
+
+            # get attention area
+            attention_area = model_vae(data)
+
+            # test the model
             recon_batch, mu, logvar = model(data)
             test_loss += loss_function(recon_batch, data, mu, logvar, args).item()
+
             if i == 0:
                 n = min(data.size(0), 8)
                 comparison = torch.cat([data[:n],
@@ -220,15 +274,23 @@ def test(model, test_loader, epoch, device, args):
 
 if __name__ == "__main__":
     # get argument
-    args, device, kwargs = argument()
+    args = argument()
+
+    ## get vae model
+    # get cnn-attention area
+    path_model_vae = os.path.join(os.getcwd(), args.path_model_vae)
+
+    # load model
+    model_vae = VAE()
+    model_vae.load_state_dict(torch.load(path_model_vae))
 
     # get image info
     info_luna16 = pd.read_csv(args.path_input, index_col=0)
     list_info_image = get_image_info(info_luna16)
 
-    len_list_info_image = len(list_info_image)
-    list_train = list_info_image[: int(len_list_info_image * args.rate_train)]
-    list_test = list_info_image[int(len_list_info_image * args.rate_train): ]
+    len_list_train = int(len(list_info_image) * args.rate_train)
+    list_train = list_info_image[: len_list_train]
+    list_test = list_info_image[len_list_train: ]
 
     # define date loader
     data_set_train = DatasetTrain(list_train)
@@ -238,22 +300,20 @@ if __name__ == "__main__":
         data_set_train,
         batch_size=args.size_batch,
         shuffle=True,
-        **kwargs,
         )
     test_loader = torch.utils.data.DataLoader(
         data_set_test,
         batch_size=args.size_batch,
         shuffle=True,
-        **kwargs,
         )
 
     # model instance
-    model = VAE(args).to(device)
+    model = CnnModel(args).to(device)
     optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
     for epoch in range(1, args.epoch + 1):
-        train(model, train_loader, epoch, device, args)
-        test(model, test_loader, epoch, device, args)
+        train(model, optimizer, model_vae, train_loader, epoch, device, args)
+        test(model, optimizer, model_vae, test_loader, epoch, device, args)
         with torch.no_grad():
             sample = torch.randn(64, args.dimension_latent).to(device)
             sample = model.decode(sample).cpu()
@@ -263,5 +323,3 @@ if __name__ == "__main__":
             save_image(
                 sample.view(64, 1, int(args.size_cutting), int(args.size_cutting)),
                 path_sample)
-    
-    torch.save(model.state_dict(), args.path_save_model)

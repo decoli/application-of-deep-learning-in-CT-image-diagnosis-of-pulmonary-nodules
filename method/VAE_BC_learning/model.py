12a806f0a57c1bb9e4b9018aa112d310f3d7de1a
張仕瑞（Shirui） <shirui.zhang.japan@gmail.com>
2020-01-10 02:14:20 +0800

    11

--- method/VAE_BC_learning/model.py
+++ method/VAE_BC_learning/model.py
@@ -10,9 +10,9 @@ class CnnModel(nn.Module):
             in_channels=1, out_channels=64, kernel_size=3, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
         ) # 64 * 30 * 30
 
-        self.conv_1_2 = nn.Conv2d(
-            in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
-        ) # 64 * 28 * 28
+        # self.conv_1_2 = nn.Conv2d(
+        #     in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        # ) # 64 * 28 * 28
 
         self.pooling_1 = nn.MaxPool2d(
             kernel_size=2, stride=2, padding=0, dilation=1, return_indices=False, ceil_mode=False
@@ -22,15 +22,15 @@ class CnnModel(nn.Module):
             in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
         ) # 128 * 12 * 12
 
-        self.conv_2_2 = nn.Conv2d(
-            in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
-        ) # 128 * 12 * 12
+        # self.conv_2_2 = nn.Conv2d(
+        #     in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'
+        # ) # 128 * 12 * 12
 
         self.pooling_2 = nn.MaxPool2d(
             kernel_size=2, stride=2, padding=0, dilation=1, return_indices=False, ceil_mode=False
         ) # 128 * 5 * 5
 
-        self.fc_1 = nn.Linear(3200, 3200)
+        # self.fc_1 = nn.Linear(3200, 3200)
 
         self.fc_2 = nn.Linear(3200, 2)
 
@@ -38,17 +38,17 @@ class CnnModel(nn.Module):
         size_batch_in = input_image.size(0)
 
         out = self.conv_1_1(input_image)
-        out = self.conv_1_2(out)
-        out = F.relu(out)
+        # out = self.conv_1_2(out)
+        # out = F.relu(out)
         out = self.pooling_1(out)
 
         out = self.conv_2_1(out)
-        out = self.conv_2_2(out)
+        # out = self.conv_2_2(out)
         out = F.relu(out)
         out = self.pooling_2(out)
 
         out = out.view(size_batch_in,-1)
-        out = self.fc_1(out)
+        # out = self.fc_1(out)
         out = F.relu(out)
         out = F.dropout(out)
         out = self.fc_2(out)

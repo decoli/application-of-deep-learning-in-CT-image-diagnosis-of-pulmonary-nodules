2ab952fce9656a967054a602e71980c8705ac92e
張仕瑞（Shirui） <shirui.zhang.japan@gmail.com>
2020-01-20 12:55:30 +0800

    11

---
+++ method/vae_recombination/image_from_decoder.py
@@ -0,0 +1,15 @@
+'''
+get the image from devoder of different epoch
+original image set is 'train set'
+
+step.1
+train the vae model. (set the epoch)
+
+model path:
+data\\model\\model_vae_random_*_cross_*_epoch_*.pt
+
+step.2
+select the image from 'train set' (important)
+feed the vae and get the image from the decoder.
+'''
+

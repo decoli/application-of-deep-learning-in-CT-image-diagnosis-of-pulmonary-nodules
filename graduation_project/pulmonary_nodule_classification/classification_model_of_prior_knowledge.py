848c31df19b26dc83b43b0e0848519b4a7274441
張仕瑞（Shirui） <shirui.zhang.japan@gmail.com>
2020-03-18 01:16:35 +0800

    11

--- graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge.py
+++ graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge.py
@@ -9,7 +9,7 @@ import torch.optim as optim
 import torch.utils.data as data
 from torch.utils.data import DataLoader
 
-class PriorKnowledgeNet(nn.modules):
+class PriorKnowledgeNet(nn.Module):
     def __init__(self):
         super(PriorKnowledgeNet, self).__init__()
 
@@ -22,6 +22,8 @@ class PriorKnowledgeNet(nn.modules):
         #
         self.conv_3_5 = nn.Conv2d(64, 128, 3, padding=1)
         self.conv_3_6 = nn.Conv2d(128, 128, 3, padding=1)
+        #
+        self.fc_3_1 = nn.Linear(2048, 512)
 
         ## kernel = 5*5
         self.conv_5_1 = nn.Conv2d(1, 32, 5, padding=2)
@@ -32,6 +34,8 @@ class PriorKnowledgeNet(nn.modules):
         #
         self.conv_5_5 = nn.Conv2d(64, 128, 5, padding=2)
         self.conv_5_6 = nn.Conv2d(128, 128, 5, padding=2)
+        #
+        self.fc_5_1 = nn.Linear(2048, 512)
 
         ## kernel = 7*7
         self.conv_7_1 = nn.Conv2d(1, 32, 7, padding=3)
@@ -42,6 +46,12 @@ class PriorKnowledgeNet(nn.modules):
         #
         self.conv_7_5 = nn.Conv2d(64, 128, 7, padding=3)
         self.conv_7_7 = nn.Conv2d(128, 128, 7, padding=3)
+        #
+        self.fc_7_1 = nn.Linear(2048, 512)
+
+        # all
+        self.fc_all_1 = nn.Linear(1536, 256)
+        self.fc_all_2 = nn.Linear(256, 2)
 
     def forward(self, x):
         size_in = x.size(0)
@@ -64,11 +74,16 @@ class PriorKnowledgeNet(nn.modules):
         out_3 = F.max_pool2d(out_3)
         out_3 = F.relu(out_3)
 
+        #
+        out_3 = out_3.view()
+        out_3 = F.dropout(out_3)
+        out_3 = self.fc_3_1(out_3)
+
         ## kernel=5*5
         out_5 = self.conv_5_1(x)
         out_5 = self.conv_5_2(out_5)
         out_5 = F.max_pool2d(out_5)
-        out_5 = F.relu(out_5)   
+        out_5 = F.relu(out_5)
 
         #
         out_5 = self.conv_5_3(out_5)
@@ -80,7 +95,12 @@ class PriorKnowledgeNet(nn.modules):
         out_5 = self.conv_5_5(out_5)
         out_5 = self.conv_5_6(out_5)
         out_5 = F.max_pool2d(out_5)
-        out_5 = F.relu(out_5)   
+        out_5 = F.relu(out_5)
+
+        #
+        out_5 = out_5.view()
+        out_5 = F.dropout(out_5)
+        out_5 = self.fc_5_1(out_5)
         
         ## kernel=7*7
         out_7 = self.conv_7_1(x)
@@ -99,3 +119,21 @@ class PriorKnowledgeNet(nn.modules):
         out_7 = self.conv_7_6(out_7)
         out_7 = F.max_pool2d(out_7)
         out_7 = F.relu(out_7)
+
+        #
+        out_7 = out_7.view()
+        out_7 = F.dropout(out_7)
+        out_7 = self.fc_7_1(out_7)
+
+        # all
+        out_all = torch.cat([out_3, out_5, out_7], dim=1)
+        out_all = self.fc_all_1(out_all)
+        out_all = F.relu(out_all)
+
+        out_all = self.fc_all_2(out_all)
+
+        #
+        return out_all
+
+model = PriorKnowledgeNet()
+print('ddd')

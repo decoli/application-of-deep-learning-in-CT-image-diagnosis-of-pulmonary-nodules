923f2c614fdcc85836319b19ff016b925fde4e9c
張仕瑞（Shirui） <shirui.zhang.japan@gmail.com>
2020-03-20 14:28:22 +0800

    11

--- graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge_advanced.py
+++ graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge_advanced.py
@@ -344,8 +344,11 @@ class DataTesting(data.Dataset):
         return return_characteristics, image_original, image_1, image_2, return_label
 
 class PriorKnowledgeNet(nn.Module):
-    def __init__(self):
+    def __init__(self, model_es):
         super(PriorKnowledgeNet, self).__init__()
+
+        ## Extracting Semantics Model
+        self.model_es = model_es
 
         ## Annotation Net
         # full connettion
@@ -402,11 +405,14 @@ class PriorKnowledgeNet(nn.Module):
         self.fc_fusion_1 = nn.Linear(384, 128)
         self.fc_fusion_2 = nn.Linear(128, 2)
 
-    def forward(self, x, x_1, x_2, x_3):
+    def forward(self, x_1, x_2, x_3):
         size_in = x_1.size(0)
 
+        # Extracting Semantics Model
+        out_es = self.model_es(x_1, x_2, x_3)
+
         # Annotation Net
-        out_ano = self.fc_1(x)
+        out_ano = self.fc_1(out_es)
         out_ano = F.relu(out_ano)
         out_ano = self.fc_2(out_ano)
         out_ano = F.relu(out_ano)
@@ -507,6 +513,135 @@ class PriorKnowledgeNet(nn.Module):
         # return out
         return out_fusion
 
+
+class ExtractingSemanticsModel(nn.Module):
+    def __init__(self):
+        super(ExtractingSemanticsModel, self).__init__()
+
+        # CNN
+        ## kernel = 3*3
+        self.conv_3_1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.conv_3_2 = nn.Conv2d(32, 32, 3, padding=1)
+        #
+        self.conv_3_3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv_3_4 = nn.Conv2d(64, 64, 3, padding=1)
+        #
+        self.conv_3_5 = nn.Conv2d(64, 128, 3, padding=1)
+        self.conv_3_6 = nn.Conv2d(128, 128, 3, padding=1)
+        #
+        self.fc_3_1 = nn.Linear(2048, 512)
+
+        ## kernel = 5*5
+        self.conv_5_1 = nn.Conv2d(1, 32, 5, padding=2)
+        self.conv_5_2 = nn.Conv2d(32, 32, 5, padding=2)
+        #
+        self.conv_5_3 = nn.Conv2d(32, 64, 5, padding=2)
+        self.conv_5_4 = nn.Conv2d(64, 64, 5, padding=2)
+        #
+        self.conv_5_5 = nn.Conv2d(64, 128, 5, padding=2)
+        self.conv_5_6 = nn.Conv2d(128, 128, 5, padding=2)
+        #
+        self.fc_5_1 = nn.Linear(2048, 512)
+
+        ## kernel = 7*7
+        self.conv_7_1 = nn.Conv2d(1, 32, 7, padding=3)
+        self.conv_7_2 = nn.Conv2d(32, 32, 7, padding=3)
+        #
+        self.conv_7_3 = nn.Conv2d(32, 64, 7, padding=3)
+        self.conv_7_4 = nn.Conv2d(64, 64, 7, padding=3)
+        #
+        self.conv_7_5 = nn.Conv2d(64, 128, 7, padding=3)
+        self.conv_7_6 = nn.Conv2d(128, 128, 7, padding=3)
+        #
+        self.fc_7_1 = nn.Linear(2048, 512)
+
+        # all
+        self.fc_all_1 = nn.Linear(1536, 1536)
+        self.fc_all_2 = nn.Linear(1536, 256)
+        self.fc_all_3 = nn.Linear(256, 40)
+
+    def forward(self, x_1, x_2, x_3):
+        size_in = x_1.size(0)
+
+        out_3 = self.conv_3_1(x_1)
+        out_3 = self.conv_3_2(out_3)
+        out_3 = F.max_pool2d(out_3, 2, 2)
+        out_3 = F.relu(out_3)
+
+        #
+        out_3 = self.conv_3_3(out_3)
+        out_3 = self.conv_3_4(out_3)
+        out_3 = F.max_pool2d(out_3, 2, 2)
+        out_3 = F.relu(out_3)
+
+        #
+        out_3 = self.conv_3_5(out_3)
+        out_3 = self.conv_3_6(out_3)
+        out_3 = F.max_pool2d(out_3, 2, 2)
+        out_3 = F.relu(out_3)
+
+        #
+        out_3 = out_3.view(size_in, -1)
+        out_3 = F.dropout(out_3)
+        out_3 = self.fc_3_1(out_3)
+
+        ## kernel=5*5
+        out_5 = self.conv_5_1(x_2)
+        out_5 = self.conv_5_2(out_5)
+        out_5 = F.max_pool2d(out_5, 2, 2)
+        out_5 = F.relu(out_5)
+
+        #
+        out_5 = self.conv_5_3(out_5)
+        out_5 = self.conv_5_4(out_5)
+        out_5 = F.max_pool2d(out_5, 2, 2)
+        out_5 = F.relu(out_5)
+
+        #
+        out_5 = self.conv_5_5(out_5)
+        out_5 = self.conv_5_6(out_5)
+        out_5 = F.max_pool2d(out_5, 2, 2)
+        out_5 = F.relu(out_5)
+
+        #
+        out_5 = out_5.view(size_in, -1)
+        out_5 = F.dropout(out_5)
+        out_5 = self.fc_5_1(out_5)
+        
+        ## kernel=7*7
+        out_7 = self.conv_7_1(x_3)
+        out_7 = self.conv_7_2(out_7)
+        out_7 = F.max_pool2d(out_7, 2, 2)
+        out_7 = F.relu(out_7)
+
+        #
+        out_7 = self.conv_7_3(out_7)
+        out_7 = self.conv_7_4(out_7)
+        out_7 = F.max_pool2d(out_7, 2, 2)
+        out_7 = F.relu(out_7)
+
+        #
+        out_7 = self.conv_7_5(out_7)
+        out_7 = self.conv_7_6(out_7)
+        out_7 = F.max_pool2d(out_7, 2, 2)
+        out_7 = F.relu(out_7)
+
+        #
+        out_7 = out_7.view(size_in, -1)
+        out_7 = F.dropout(out_7)
+        out_7 = self.fc_7_1(out_7)
+
+        # all
+        out_all = torch.cat([out_3, out_5, out_7], dim=1)
+        out_all = self.fc_all_1(out_all)
+        out_all = F.dropout(out_all)
+        out_all = self.fc_all_2(out_all)
+        out_all = F.relu(out_all)
+        out_all = self.fc_all_3(out_all)
+
+        return out_all
+
+
 print('ddd')
 # get train and test data
 num_training = int(len(list_data) * RATE_TRAIN)
@@ -519,8 +654,13 @@ data_testing = DataTesting(list_data_testing)
 data_loader_training = DataLoader(data_training, batch_size=BATCH_SIZE, shuffle=True)
 data_loader_testing = DataLoader(data_testing, batch_size=BATCH_SIZE, shuffle=True)
 
+# get ES model (extracting semantics model)
+model_es = ExtractingSemanticsModel()
+model_es.load_state_dict(torch.load('model_extracting_semantics.pt'))
+
+
 # get model
-model = PriorKnowledgeNet().to(DEVICE)
+model = PriorKnowledgeNet(model_es).to(DEVICE)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.01)
 

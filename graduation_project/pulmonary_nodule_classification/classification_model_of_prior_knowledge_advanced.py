3ecc4914464a0e87b43f2e8e7074e8624d542461
unknown <shirui.zhang.china@gmail.com>
2020-03-22 01:24:54 +0800

    11

--- graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge_advanced.py
+++ graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge_advanced.py
@@ -29,7 +29,7 @@ from utility.visdom import (visdom_acc, visdom_loss, visdom_roc_auc, visdom_se,
 
 
 BATCH_SIZE=256
-EPOCHS=200
+EPOCHS=150
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu") # 让torch判断是否使用GPU，建议使用GPU环境，因为会快很多
 RATE_TRAIN = 0.8
 root_image = 'data/dataset_deep_lung/data_sample/png'
@@ -118,7 +118,7 @@ class DataTraining(data.Dataset):
         image_original = cv2.imread(path_image, flags=2)
         image_copy_1 = image_original.copy()
         image_copy_2 = image_original.copy()
-        cv2.imwrite('image_oririnal.png', image_original)
+        # cv2.imwrite('image_oririnal.png', image_original)
         image_original = torch.Tensor(image_original)
         image_original = torch.unsqueeze(image_original, 0)
 
@@ -126,17 +126,17 @@ class DataTraining(data.Dataset):
         seeds = [Point(15,15), Point(16,15), Point(15,16), Point(16,16)]
         mask_1 = regionGrow(image_copy_1, seeds, 10)
         mask_2 = regionGrow(image_copy_2, seeds, 20)
-        cv2.imwrite('mask_1.png', mask_1 * 255)
-        cv2.imwrite('mask_2.png', mask_2 * 255)
+        # cv2.imwrite('mask_1.png', mask_1 * 255)
+        # cv2.imwrite('mask_2.png', mask_2 * 255)
 
         # get image masked (and transfered to Tensor)
         image_copy_1[mask_1==0] = [0]
-        cv2.imwrite('image_copy_1.png', np.array(image_copy_1))
+        # cv2.imwrite('image_copy_1.png', np.array(image_copy_1))
         image_1 = torch.Tensor(image_copy_1)
         image_1 = torch.unsqueeze(image_1, 0)
 
         image_copy_2[mask_2==0] = [0]
-        cv2.imwrite('image_copy_2.png', np.array(image_copy_2))
+        # cv2.imwrite('image_copy_2.png', np.array(image_copy_2))
         image_2 = torch.Tensor(image_copy_2)
         image_2 = torch.unsqueeze(image_2, 0)
 
@@ -165,7 +165,7 @@ class DataTesting(data.Dataset):
         image_original = cv2.imread(path_image, flags=2)
         image_copy_1 = image_original.copy()
         image_copy_2 = image_original.copy()
-        cv2.imwrite('image_oririnal.png', image_original)
+        # cv2.imwrite('image_oririnal.png', image_original)
         image_original = torch.Tensor(image_original)
         image_original = torch.unsqueeze(image_original, 0)
 
@@ -173,17 +173,17 @@ class DataTesting(data.Dataset):
         seeds = [Point(15,15), Point(16,15), Point(15,16), Point(16,16)]
         mask_1 = regionGrow(image_copy_1, seeds, 10)
         mask_2 = regionGrow(image_copy_2, seeds, 20)
-        cv2.imwrite('mask_1.png', mask_1 * 255)
-        cv2.imwrite('mask_2.png', mask_2 * 255)
+        # cv2.imwrite('mask_1.png', mask_1 * 255)
+        # cv2.imwrite('mask_2.png', mask_2 * 255)
 
         # get image masked (and transfered to Tensor)
         image_copy_1[mask_1==0] = [0]
-        cv2.imwrite('image_copy_1.png', np.array(image_copy_1))
+        # cv2.imwrite('image_copy_1.png', np.array(image_copy_1))
         image_1 = torch.Tensor(image_copy_1)
         image_1 = torch.unsqueeze(image_1, 0)
 
         image_copy_2[mask_2==0] = [0]
-        cv2.imwrite('image_copy_2.png', np.array(image_copy_2))
+        # cv2.imwrite('image_copy_2.png', np.array(image_copy_2))
         image_2 = torch.Tensor(image_copy_2)
         image_2 = torch.unsqueeze(image_2, 0)
 

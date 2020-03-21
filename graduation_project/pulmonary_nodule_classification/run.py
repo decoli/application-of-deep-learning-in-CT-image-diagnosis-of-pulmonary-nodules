a8dcae0a4bcda36b238defbe847bc6dce9bb05e0
unknown <shirui.zhang.china@gmail.com>
2020-03-22 00:18:40 +0800

    11

--- graduation_project/pulmonary_nodule_classification/run.py
+++ graduation_project/pulmonary_nodule_classification/run.py
@@ -2,5 +2,5 @@ import os
 
 for time in range(10):
     for use_cross in range(1, 5):
-        run_command = 'graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge_advanced.py --use-cross {use_cross}'.format(use_cross=use_cross + 1)
+        run_command = 'python graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge_advanced.py --use-cross {use_cross}'.format(use_cross=use_cross + 1)
         os.system(run_command)

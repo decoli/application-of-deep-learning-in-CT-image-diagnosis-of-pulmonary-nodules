eb8ff259d03c9502a1a4d991ed8364e36f9e7dc3
unknown <shirui.zhang.china@gmail.com>
2020-04-07 19:40:02 +0800

    11

--- graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge_advanced.py
+++ graduation_project/pulmonary_nodule_classification/classification_model_of_prior_knowledge_advanced.py
@@ -30,7 +30,7 @@ from utility.visdom import (visdom_acc, visdom_loss, visdom_roc_auc, visdom_se,
 
 
 BATCH_SIZE=256
-EPOCHS=300
+EPOCHS=500
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu") # 让torch判断是否使用GPU，建议使用GPU环境，因为会快很多
 RATE_TRAIN = 0.8
 root_image = 'data/dataset_deep_lung/data_sample/png'

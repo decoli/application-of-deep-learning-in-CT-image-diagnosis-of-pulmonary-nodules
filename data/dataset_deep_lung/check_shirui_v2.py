ac240b75d5f268ec76368c634783b5a7aebc34c4
unknown <shirui.zhang.china@gmail.com>
2020-04-17 16:15:11 +0800

    11

--- data/dataset_deep_lung/check_shirui_v2.py
+++ data/dataset_deep_lung/check_shirui_v2.py
@@ -1,13 +1,17 @@
 import pandas as pd
 
-path_shirui_v2 = 'data/dataset_deep_lung/annotationdetclssgm_doctor_shirui_v2.csv'
+path_shirui_v2 = 'data/dataset_deep_lung/annotationdetclssgm_doctor_shirui_v2_test.csv'
 
 pd_shirui_v2 = pd.read_csv(path_shirui_v2)
-count = 0
+count_1 = 0
+count_2 = 0
 for index, each_annotation in pd_shirui_v2.iterrows():
     condition_1 = each_annotation['malignant'] == 1 and each_annotation['malignancy'] < 3
     condition_2 = each_annotation['malignant'] == 0 and each_annotation['malignancy'] > 3
-    if condition_1 or condition_2:
-        count += 1
-        print(each_annotation)
-print(count)
+    if condition_1:
+        count_1 += 1
+    if condition_2:
+        count_2 += 1
+
+print(count_1)
+print(count_2)
